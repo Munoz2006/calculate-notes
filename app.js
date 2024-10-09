2078be17a6554d8af85aae9a206d0cfc725e7ee8
David Muñoz <davidmunozmoreno2@gmail.com>
2024-10-09 10:49:46 -0500

    feat: finished

--- app.js
+++ app.js
@@ -13,7 +13,8 @@
   const btnCalculate = document.getElementById('btn-calculate')
   const response = document.getElementById('resultado')
   const btnPredict = document.getElementById('predecir')
-  
+  const response1 = document.getElementById('predecir-R')
+
   btnCalculate.addEventListener('click', calculateNote )
   btnPredict.addEventListener('click', predict)
 
@@ -27,23 +28,29 @@
     
     let result = ((note1 * 0.3) + (note2 * 0.3) + (note3 * 0.4)).toFixed(2)
     // template string o template literal
-    if(result< 3.5){
+    if(note3<=5){
+      if(result< 3.5){
         response.style.color = 'black'
         response.textContent = `SR/SRA/SRE ${username.value} su nota definitiva es: ${result}, perdio la materia`    
-
-    }else if(result >= 3.5 && result <= 4.5){
+      }else if(result >= 3.5 && result <= 4.5){
         response.style.color = 'orange'
         response.textContent = `SR/SRA/SRE ${username.value} su nota definitiva es: ${result}, gano la materia`
-    }else if(result > 4.5){
+      }else if(result > 4.5){
         response.style.color = 'green'
         response.textContent = `SR/SRA/SRE ${username.value} su nota definitiva es: ${result}, felicidades es sobresaliente`
+      }
     }
   }
 
   function predict(){
-    const nota3 = 3.5;
-    let nota1 = Number(data1.value)
-    let nota2 = Number(data1.value)
-    let total = ((nota3 - (0.3 * nota1 + 0.3 * nota2)) / 0.4).toFixed(2) 
-    data3.value = total;
+    const notaAprobatoria = 3.5; 
+    let nota1 = Number(data1.value);  
+    let nota2 = Number(data2.value);  
+
+    let required = (notaAprobatoria - (nota1 * 0.3 + nota2 * 0.3)); 
+    let total= required / 0.4
+    data3.value = total.toFixed(2);
+    if (total> 5) {
+      response1.textContent= `No es posible que gane la materia ya que la nota 3 es mayor a 5`    
+    }
   }
